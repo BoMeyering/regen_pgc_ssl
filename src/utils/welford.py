495de6b5b1046586cd983f24e0690e253b099a86
BoMeyering <bomeyering@mailbox.org>
2024-05-04 00:11:57 -0400

    Finalized dataset mean_and_std processing script

--- src/utils/welford.py
+++ src/utils/welford.py
@@ -1,64 +1,33 @@
 # Welfords method for calculating mean and variance
 # BoMeyering 2024
 import torch
+import sys
 
 class WelfordCalculator:
     def __init__(self):
-        self.M = torch.zeros(3)
-        self.S = torch.zeros(3)
-        self.N = 0
+        self.M = torch.zeros(3, dtype=torch.float64)
+        self.M_old = torch.zeros(3, dtype=torch.float64)
+        self.S = torch.zeros(3, dtype=torch.float64)
+        self.N = torch.zeros(1, dtype=torch.long)
 
-    def _update(self, array: torch.Tensor):
-        if len(array.shape) > 3:
-            raise ValueError(f"Passed tensor should have shape of len=3. Current 'array' argument is of shape {tensor.shape}")
-        elif array.shape[0] != 3:
-            raise ValueError(f"Argument 'array' should be a 3 channel image. Tensor passed has {tensor.shape[0]} channels.")
+    def update(self, array: torch.Tensor):
+        if (len(array.shape) > 3) or (array.shape[0] != 3):
+            raise ValueError(f"Passed tensor should be a 3 channel image of shape (3, H, W). Current 'array' argument is of shape {array.shape}")
+        
+        # Update count based on number of pixels
         num_px = array.shape[1] * array.shape[2]
         self.N += num_px
-        old_M = self.M.clone()
-        M_new = array.mean(dim=(1, 2))
-        
-        D1 = M_new - self.M
-        self.M += D1 * num_px / self.N
-        # print("M: ", self.M)
 
-        # S_term = (array - M_new.unsqueeze(-1).unsqueeze(-1)) * (array - self.M.unsqueeze(-1).unsqueeze(-1))
-        # self.S += S_term.sum(dim=(1, 2))
-        
-        array_reshaped = array.reshape(3, -1)  # Reshape to (3, H*W)
-        old_M_expanded = old_M.unsqueeze(-1).expand_as(array_reshaped)
-        M_new_expanded = M_new.unsqueeze(-1).expand_as(array_reshaped)
-        self.S += ((array_reshaped - old_M_expanded) * (array_reshaped - M_new_expanded)).sum(dim=1)
+        # Calculate array mean and update running mean
+        self.M_old = self.M.clone()
+        m_new = array.mean(dim=(1, 2))
+        self.M += (m_new - self.M) * num_px / self.N
 
-        # print("New Terms", self.M, self.S)
+        # Calculate new S incremental term (x-M_t) * (x-M_{t-1})
+        S_incr = (array - self.M.unsqueeze(-1).unsqueeze(-1)) * (array - self.M_old.unsqueeze(-1).unsqueeze(-1))
+        self.S += S_incr.sum(dim=(1, 2))
 
     def compute(self):
-        variance = self.S / self.N
-        std = torch.sqrt(variance)
+        self.std = torch.sqrt(self.S / self.N)
 
-        return self.M, std
-
-if __name__ == '__main__':
-    welford = WelfordCalculator()
-
-    means = torch.zeros(3)
-    std = torch.zeros(3)
-    pixel_n = torch.zeros(1)
-
-    for i in range(100000):
-        tensor = torch.randn(3, 10, 10)+10
-        welford._update(tensor)
-
-        channel_means = torch.sum(tensor, dim=(1, 2))
-        channel_std = torch.sum(tensor** 2, dim=(1, 2))
-        pixels = tensor.shape[2] * tensor.shape[1]
-
-        means += channel_means
-        std += channel_std
-        pixel_n += pixels
-
-        if i % 1000 == 0:
-            mean, variance = welford.compute()
-            print(mean,variance)
-
-            print(means / pixel_n, channel_std * pixel_n / i)
+        return self.M, self.std
