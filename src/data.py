25ad03a6b3405106ea4ca8b650f30c3899c47131
BoMeyering <bomeyering@mailbox.org>
2024-05-06 01:16:16 -0400

    Updated README

--- src/data.py
+++ src/data.py
@@ -6,7 +6,7 @@ import albumentations as A
 from PIL import Image
 from scipy.io import loadmat
 from torch.utils.data import Dataset
-from typing import Union
+from typing import Union, Any
 from pathlib import Path
 from glob import glob
 
@@ -51,3 +51,42 @@ class StatDataset(Dataset):
     
     def __len__(self):
         return len(self.image_keys)
+    
+class LabeledDataset(Dataset):
+    def __init__(self, img_dir: Union[Path, str], target_dir: Union[Path, str], transforms: A.Compose):
+        self.img_dir = img_dir
+        self.target_dir = target_dir
+        self.transforms = transforms
+
+        if not os.path.exists(self.img_dir):
+            raise NotADirectoryError(f'Path to img_dir {self.img_dir} does not exist. Please check path integrity.')
+        elif not os.path.exists(self.target_dir):
+            raise NotADirectoryError(f'Path to target_dir {self.target_dir} does not exist. Please check path integrity.')
+
+        self.img_keys = sorted([img for img in glob('*', root_dir = self.img_dir) if img.endswith(('jpg'))])
+        self.target_keys = sorted([t for t in glob('*', root_dir = self.target_dir) if t.endswith(('png'))])
+
+        if len(self.img_keys) != len(self.target_keys):
+            raise ValueError(f"Image keys and target keys are different lengths. Please ensure that each training image in {self.img_dir} has a corresponding target mask in {self.target_dir}")
+        elif set(self.img_keys.replace('.jpg', '')) != set(self.target_keys.replace('.png', '')):
+            raise ValueError(f"Base names for 'img_keys' and 'target_keys' do not match. Please ensure that each image in {self.img_dir} has a corresponding target mask in {self.target_dir} with the same base name.")
+
+        
+        
+
+        
+    def __getitem__(self, index) -> Any:
+
+        img_key = self.img_keys[index]
+        target_key = 
+
+
+
+
+        return img, target
+    
+
+    def __len__(self):
+
+        return len(self.img)
+    
