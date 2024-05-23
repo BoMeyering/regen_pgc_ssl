977a4cf8ffaa1d84188f53a2a81bd36fe0b83329
BoMeyering <bomeyering@mailbox.org>
2024-05-22 21:05:48 -0400

    Updated configs for loss experiment

--- scripts/run_loss_experiments.sh
+++ scripts/run_loss_experiments.sh
@@ -8,5 +8,7 @@ python train.py configs/train_config_cb_focal_loss.yaml
 python train.py configs/train_config_acb_ce_loss.yaml
 python train.py configs/train_config_acb_focal_loss.yaml
 python train.py configs/train_config_recall_loss.yaml
+python train.py configs/train_config_dice_loss.yaml
+python train.py configs/train_config_tversky_loss.yaml
 
 exit 0
